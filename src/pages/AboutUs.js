b7eb3f0ba8bfa264bebf767be5a6ba9b5d435568
Kacueto <75464308+Kacueto@users.noreply.github.com>
2024-07-31 01:07:44 +0000

    changes in AboutUs

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -6,24 +6,30 @@ import useHandleNavigation from '../hooks/useHandleNavigation';
 const AboutUs = () => {
     const handleNavigation = useHandleNavigation();
     return (
-        
-        <div className="homeaboutus-container">
-            <div className="aboutus-container1">
+
+        <article className="homeaboutus-container">
+            <section className="aboutus-container1">
                 <h1>What is this<br></br> club?</h1>
                 <div className="aboutus-container1-content">
                     <p>
-                    We are twins and the purpose of this club is to teach Spanish to everyone who decides to be part of this club.
+                        We are twins and the purpose of this club is to teach Spanish to everyone who decides to be part of this club.
                     </p>
-                    <button  className="aboutus-container1-button" onClick={() => handleNavigation('/ContactUs')}>Contact Us</button>
+                    <button className="aboutus-container1-button" onClick={() => handleNavigation('/ContactUs')}>Contact Us</button>
                     <button className="aboutus-container1-button1">Know the twins</button>
                 </div>
-            </div>
-            <div className="aboutus-container">
-                <h1>About Us</h1>
-                <p>Spanish Club With The Twins is a platform that aims to help people learn Spanish in a fun and interactive way. Our goal is to provide you with the tools and resources you need to become fluent in Spanish. Whether you are a beginner or an advanced learner, we have something for everyone. Our courses are designed to help you improve your listening, speaking, reading, and writing skills. We also offer a podcast where you can listen to native speakers and practice your listening skills. So why wait? Join Spanish Club With The Twins today and start your journey to fluency!</p>
-            </div>
+            </section>
+            
+            <section className="aboutus-container">
+                <h1>The twins and their leanguages</h1>
+                <p>Hello everyone, we are the twins Christian and Camilo Ochoa. We were born in Córdoba (Bolivar) in north Colombia.  We are political scientists Graduated with an emphasis in public policies from the University del Norte Barranquilla. Since, we were children we were fascinated by the cultures of the world and little by little we noticed that our fascination grew more and more to the point of mastering 5 languages: Spanish (Native lenguage), English, Portuguese, German and Italian.
+                    We are currently dedicated to business and political consultancy. In addition to that, we teach English part-time which we really love very much.
+                    Our love for teaching and foreign languages ​has grown so much that together we have decided to launch this project to share with you our knowledge and methods to learn and understand about our beautiful Ibero-Maerican culture.
+                    We would like to dedicate ourselves wholeheartedly to this project full time, which is why we truly appreciate your donations and count on you to support this proyect and make it grow.
+                    Thank you very much!
+                    The Twins Christian and Camilo Ochoa.</p>
+            </section>
             <Footer />
-        </div>
+        </article>
     );
 
 }
